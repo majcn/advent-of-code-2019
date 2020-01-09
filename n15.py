69aad4d96d06f317852ead7247d8e9c38e8022db
Gregor Majcen <gregor.majcen@sinergise.com>
2020-01-09 16:09:15 +0100

    use recursion for moving (n15)

--- n15.py
+++ n15.py
@@ -104,56 +104,61 @@ def parse_data():
     return [int(x) for x in input_data.split(',')]
 
 
-def get_grid(data):
-    program_input = []
-    program = IntcodeRunner(data, iter(program_input))
-
+def move_robot(program, program_input, cmd):
     output_moved = 1
     output_goal = 2
 
+    program_input.append(cmd)
+    n_p_output_state = program.run()
+
+    if n_p_output_state == output_moved or n_p_output_state == output_goal:
+        return True, n_p_output_state == output_goal
+
+    return False, False
+
+
+def explore(location, send_move_cmd_f, visited=None):
+    grid = set()
+    goal = set()
+
+    if visited is None:
+        visited = set()
+
+    neighbors = (
+        ((location[0], location[1] - 1), 1, 2),
+        ((location[0], location[1] + 1), 2, 1),
+        ((location[0] - 1, location[1]), 3, 4),
+        ((location[0] + 1, location[1]), 4, 3)
+    )
+
+    for n_location, n_p_input, n_r_p_input in neighbors:
+        if n_location in visited:
+            continue
+
+        has_moved, found_goal = send_move_cmd_f(n_p_input)
+        visited.add(n_location)
+
+        if found_goal:
+            goal.add(n_location)
+
+        if has_moved:
+            grid.add(n_location)
+            n_grid, n_goal = explore(n_location, send_move_cmd_f, visited)
+            grid.update(n_grid)
+            goal.update(n_goal)
+
+            send_move_cmd_f(n_r_p_input)
+
+    return grid, goal
+
+
+def get_grid(data):
     start = (0, 0)
-    goal = None
-    grid = {start}
+    program_input = []
+    program = IntcodeRunner(data, iter(program_input))
+    grid, goal = explore(start, lambda x: move_robot(program, program_input, x))
 
-    discovered = {start}
-    came_from = {}
-
-    location = start
-    while True:
-        neighbors = (
-            ((location[0], location[1] - 1), 1, 2),
-            ((location[0], location[1] + 1), 2, 1),
-            ((location[0] - 1, location[1]), 3, 4),
-            ((location[0] + 1, location[1]), 4, 3)
-        )
-
-        has_changed_location = False
-        for n_location, n_p_input, n_r_p_input in neighbors:
-            if n_location in discovered:
-                continue
-
-            discovered.add(n_location)
-
-            program_input.append(n_p_input)
-            n_p_output_state = program.run()
-
-            if n_p_output_state == output_goal:
-                goal = n_location
-
-            if n_p_output_state == output_moved or n_p_output_state == output_goal:
-                grid.add(n_location)
-                came_from[n_location] = (location, n_r_p_input)
-                location = n_location
-                has_changed_location = True
-                break
-
-        if not has_changed_location:
-            if location == start:
-                return start, goal, grid
-
-            location, n_r_p_input = came_from[location]
-            program_input.append(n_r_p_input)
-            program.run()
+    return start, goal.pop(), grid
 
 
 def get_valid_neighbors(grid, location):
